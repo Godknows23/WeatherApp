df725c872c2943f28b140d3c07b5c17ba9b63f91
Godknows@23 <godknows@velocityinc.tech>
2023-01-19 16:50:30 +0200

    calling the  modules to the index file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import weather from "./modules/weather";
-import view from "./modules/view";
+ import weather from "./modules/weather";
+ import view from "./modules/view";
 
-const searchForm = document.getElementById("searchForm");
-const searchInput = document.getElementById("searchInput");
-const searchBtn = document.getElementById("searchBtn");
+ const searchForm = document.getElementById("searchForm");
+ const searchInput = document.getElementById("searchInput");
+ const searchBtn = document.getElementById("searchBtn");
 
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+ searchForm.addEventListener("submit", (e) => {
+   e.preventDefault();
 });
 
-searchBtn.addEventListener("click", async () => {
-  if (searchInput.value === "") return;
-  const weatherData = await weather.getData(searchInput.value);
-  view.setSearchResult(weatherData);
+ searchBtn.addEventListener("click", async () => {
+   if (searchInput.value === "") return;
+   const weatherData = await weather.getData(searchInput.value);
+   view.setSearchResult(weatherData);
 });
